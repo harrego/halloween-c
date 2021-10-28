25f20ad2d5b69c52755eeeece664daffa3cdab89
megabytesofrem <dhillonwilk@gmail.com>
2021-10-28 14:35:30 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#define HALLOWEEN_UNIX_TIMESTAMP 1635683760
+
+int main(void)
+{
+  int spoopy_date = (int)time(NULL);
+  int is_halloween = 0;
+  
+  if (spoopy_date == HALLOWEEN_UNIX_TIMESTAMP) {
+    is_halloween = 1;
+    printf("happy spoopy month\n");
+  }  
+}
